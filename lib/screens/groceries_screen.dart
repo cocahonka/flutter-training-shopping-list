56e156daf2a3a614335c8c101798bdb9f8536a51
Ivan <cocahonka@gmail.com>
2023-11-24 17:31:27 +0300

    made a start layout

--- lib/screens/groceries_screen.dart
+++ lib/screens/groceries_screen.dart
@@ -1,13 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_list/data/dummy_items.dart';
 
 class GroceriesScreen extends StatelessWidget {
   const GroceriesScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Groceries'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Groceries'),
+      ),
+      body: ListView.builder(
+        itemCount: kGroceryItems.length,
+        itemBuilder: (context, index) {
+          final grocery = kGroceryItems[index];
+          return ListTile(
+            key: ValueKey(grocery),
+            leading: Container(
+              width: 25,
+              height: 25,
+              color: grocery.category.color,
+            ),
+            title: Text(grocery.name),
+            trailing: Text(grocery.quantity.toString()),
+          );
+        },
       ),
     );
   }
