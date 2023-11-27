5f2f928ccacb2a0678c50080a6fa9c0a2c9a26ce
Ivan <cocahonka@gmail.com>
2023-11-27 10:09:50 +0300

    added a form for new item

--- lib/screens/new_item_screen.dart
+++ lib/screens/new_item_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_list/extensions/capitalize.dart';
+import 'package:shopping_list/models/category.dart';
 
 class NewItemScreen extends StatelessWidget {
   const NewItemScreen({super.key});
@@ -11,7 +13,65 @@ class NewItemScreen extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(12),
-        child: Text('temp text'),
+        child: Form(
+          child: Column(
+            children: [
+              TextFormField(
+                maxLength: 50,
+                decoration: const InputDecoration(
+                  label: Text('Name'),
+                ),
+                validator: (value) {
+                  return 'Demo...';
+                },
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      keyboardType: TextInputType.number,
+                      decoration: const InputDecoration(
+                        label: Text('Quantity'),
+                      ),
+                      initialValue: '1',
+                      validator: (value) {
+                        return 'Demo...';
+                      },
+                    ),
+                  ),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 8),
+                      child: DropdownButtonFormField(
+                        items: [
+                          for (final category in Category.values)
+                            DropdownMenuItem(
+                              value: category,
+                              child: Row(
+                                children: [
+                                  Container(
+                                    width: 16,
+                                    height: 16,
+                                    color: category.color,
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.only(left: 6),
+                                    child: Text(category.name.capitalize()),
+                                  ),
+                                ],
+                              ),
+                            ),
+                        ],
+                        onChanged: (value) {},
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
